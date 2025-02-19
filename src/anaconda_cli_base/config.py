4a184252448ec46883eb4b98b075062c92b31c44
Albert DeFusco <adefusco@anaconda.com>
2025-02-19 11:15:29 -0500

    streamline model_config setup
    * with tuple support
    * error handling

--- src/anaconda_cli_base/config.py
+++ src/anaconda_cli_base/config.py
@@ -6,6 +6,7 @@ from typing import Any
 from typing import Optional
 from typing import Tuple
 from typing import Type
+from typing import Union
 
 from pydantic_settings import BaseSettings
 from pydantic_settings import PydanticBaseSettingsSource
@@ -24,31 +25,34 @@ def anaconda_config_path() -> Path:
 
 
 class AnacondaBaseSettings(BaseSettings):
+
     def __init_subclass__(
-        cls, plugin_name: Optional[str] = None, **kwargs: Any
+        cls, plugin_name: Optional[Union[str, tuple]] = None, **kwargs: Any
     ) -> None:
+        base_env_prefix: str = "ANACONDA_"
+        if plugin_name is None:
+            pyproject_toml_table_header = ()
+            env_prefix = base_env_prefix
+        if isinstance(plugin_name, tuple):
+            if not all(isinstance(entry, str) for entry in plugin_name):
+                raise ValueError(f"plugin_name={plugin_name} error: All values must be strings.")
+            pyproject_toml_table_header = ("plugin", *plugin_name)
+            env_prefix = base_env_prefix + "_".join(plugin_name).upper() + "_"
+        elif isinstance(plugin_name, str):
+            pyproject_toml_table_header = ("plugin", plugin_name)
+            env_prefix = base_env_prefix + f"{plugin_name.upper()}_"
+        else:
+            raise ValueError(f"plugin_name={plugin_name} is not supported. It must be either a list or tuple.")
+
         cls.model_config = SettingsConfigDict(
             env_file=".env",
-            pyproject_toml_table_header=(),
-            env_prefix="ANACONDA_",
+            pyproject_toml_table_header=pyproject_toml_table_header,
+            env_prefix=env_prefix,
             env_nested_delimiter="__",
             extra="ignore",
             ignored_types=(cached_property,),
         )
 
-        cls.model_config["pyproject_toml_table_header"] = (
-            ()
-            if plugin_name is None
-            else (
-                "plugin",
-                plugin_name,
-            )
-        )
-        base_env_prefix = cls.model_config.get("env_prefix", "")
-        env_prefix = base_env_prefix + (
-            "" if plugin_name is None else f"{plugin_name.upper()}_"
-        )
-        cls.model_config["env_prefix"] = env_prefix
         return super().__init_subclass__(**kwargs)
 
     @classmethod
