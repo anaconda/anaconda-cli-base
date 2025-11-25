78fc87a0ceeb243f100ba305498ec2cfb5885ba5
mattkram <mkramer@anaconda.com>
2025-11-25 10:25:54 -0600

    refac: Remove wrapper function

--- src/anaconda_cli_base/console.py
+++ src/anaconda_cli_base/console.py
@@ -2,9 +2,7 @@ import logging
 import os
 from typing import List
 
-import click
-import readchar
-from readchar import key
+from readchar import key, readkey
 from rich.console import Console
 from rich.live import Live
 from rich.logging import RichHandler
@@ -52,21 +50,6 @@ def _generate_table(header: str, rows: List[str], selected: int) -> Table:
     return table
 
 
-def _read_key() -> str:
-    """Read a key from the terminal. We use click if we are not on Windows, but must
-    use `readchar.readkey()` on Windows since keys like UP/DOWN are multiple characters.
-
-    We can probably just use readkey() for all OS's, but that is proving challenging to mock.
-    """
-    return readchar.readkey()
-    try:
-        import msvcrt  # noqa: F401
-    except ImportError:
-        return click.getchar()
-    else:
-        return readchar.readkey()
-
-
 def select_from_list(prompt: str, choices: List[str]) -> str:
     """Dynamically select from a list of choices, by using the up/down keys."""
     # inspired by https://github.com/Textualize/rich/discussions/1785#discussioncomment-1883808
@@ -74,7 +57,7 @@ def select_from_list(prompt: str, choices: List[str]) -> str:
 
     selected = 0
     with Live(_generate_table(prompt, items, selected), auto_refresh=False) as live:
-        while keypress := _read_key():
+        while keypress := readkey():
             if keypress == key.UP or keypress == "k":
                 selected = max(0, selected - 1)
             if keypress == key.DOWN or keypress == "j":
