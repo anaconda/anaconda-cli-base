5aaacb92675aa309821943a5c38036ba2adfb777
Albert DeFusco <adefusco@anaconda.com>
2026-01-13 10:57:00 -0500

    fix and test

--- src/anaconda_cli_base/cli.py
+++ src/anaconda_cli_base/cli.py
@@ -62,7 +62,7 @@ class ErrorHandledGroup(TyperGroup):
                     **extra,
                 )
             else:
-                cmd = " ".join(ctx.args)
+                cmd = " ".join(args)
                 console.print(
                     f"\nTo see a more detailed error message run the command again as"
                     f"\n  [green]anaconda --verbose {cmd}[/green]"
